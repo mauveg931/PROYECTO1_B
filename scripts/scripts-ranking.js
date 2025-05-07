2062acece2d15b3131fe60bfb275148c6310a40f
zeltiaferver <fvereszeltia@gmail.com>
2025-05-07 17:22:02 +0200

    fix

--- scripts/scripts-ranking.js
+++ scripts/scripts-ranking.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
 /**
  * calcular puntuaciones
  */
+const movimientos = document.getElementById('movimientos');
+const aciertos = document.getElementById('aciertos'); 
 
 function calcularPuntos(aciertos, movimientos, tiempo) {
   var puntosFinales = (aciertos * 10) - (movimientos * 5) - (tiempo*0.5) ;
