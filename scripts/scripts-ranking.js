4de92068a0754c244e8addae6828100e35331197
zeltiaferver <fvereszeltia@gmail.com>
2025-05-07 17:11:27 +0200

    Create scripts-ranking.js

---
+++ scripts/scripts-ranking.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+/**
+ * calcular puntuaciones
+ */
+
+function calcularPuntos(aciertos, intentos, tiempo) {
+  var puntosFinales = (aciertos * 10) - (intentos * 5) - (tiempo*0.5) ;
+
+
+}
+
+
+});
