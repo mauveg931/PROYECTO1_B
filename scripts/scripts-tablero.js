fc00103a30169e1027a804b2dec7637e37c9813b
Hugovg2791 <hugovila2710@gmail.com>
2025-05-09 17:40:35 +0200

    bakup

--- scripts/scripts-tablero.js
+++ scripts/scripts-tablero.js
@@ -262,7 +262,7 @@ campojuego.addEventListener("click", () => {
         function mostrarCruzVisual(carta) {
             const cruz = document.createElement('div');
             cruz.className = 'cruz-error';  
-            cruz.textContent = '❌';  
+            cruz.textContent = 'X';  
             carta.appendChild(cruz);  
 
             setTimeout(() => {
